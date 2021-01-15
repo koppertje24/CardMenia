4d7e05f3aeb5e3a5c619413272b003d63f446bdc
koppertje24 <32428243+koppertje24@users.noreply.github.com>
2021-01-15 14:19:41 +0100

    Add files via upload

--- Build/WebGL.framework.js
+++ Build/WebGL.framework.js
@@ -1296,7 +1296,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2485456;
+STATICTOP = STATIC_BASE + 2474064;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3226,7 +3226,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2485456;
+var STATIC_BUMP = 2474064;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -3532,10 +3532,6 @@ function _JS_SystemInfo_GetPreferredDevicePixelRatio() {
 function _JS_SystemInfo_GetScreenSize(outWidth, outHeight) {
  HEAPF64[outWidth >> 3] = Module.SystemInfo.width;
  HEAPF64[outHeight >> 3] = Module.SystemInfo.height;
-}
-function _JS_SystemInfo_GetStreamingAssetsURL(buffer, bufferSize) {
- if (buffer) stringToUTF8(Module.streamingAssetsUrl, buffer, bufferSize);
- return lengthBytesUTF8(Module.streamingAssetsUrl);
 }
 function _JS_SystemInfo_HasCursorLock() {
  return Module.SystemInfo.hasCursorLock;
@@ -17540,7 +17536,6 @@ Module.asmLibraryArg = {
  "_JS_SystemInfo_GetOS": _JS_SystemInfo_GetOS,
  "_JS_SystemInfo_GetPreferredDevicePixelRatio": _JS_SystemInfo_GetPreferredDevicePixelRatio,
  "_JS_SystemInfo_GetScreenSize": _JS_SystemInfo_GetScreenSize,
- "_JS_SystemInfo_GetStreamingAssetsURL": _JS_SystemInfo_GetStreamingAssetsURL,
  "_JS_SystemInfo_HasCursorLock": _JS_SystemInfo_HasCursorLock,
  "_JS_SystemInfo_HasFullscreen": _JS_SystemInfo_HasFullscreen,
  "_JS_SystemInfo_HasWebGL": _JS_SystemInfo_HasWebGL,
